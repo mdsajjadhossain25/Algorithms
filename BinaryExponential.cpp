fa86a49651705343af5f9c19a8582f6db17eb3d3
Md Sajjad Hossain <81988769+mdsajjadhossain25@users.noreply.github.com>
2023-05-28 14:45:41 +0600

    Update BinaryExponential.cpp
    new code

--- BinaryExponential.cpp
+++ BinaryExponential.cpp
@@ -1,10 +1,12 @@
+const int m= 1e9+7;
+
 int binExpo(int a, int b){
     int ans = 1;
     while(b){
         if(b&1){
-            ans = ans * a;
+            ans = (ans * 1LL * a)%m;
         }
-        a = a * a;
+        a = (a * 1LL* a)%m;
         b >>=1;
     }
     return ans;
